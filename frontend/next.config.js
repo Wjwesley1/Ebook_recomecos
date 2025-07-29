55ab5345f3454f7493cef30198534309f6cdc32f
wesley <wesleydejesusalvarenga@gmail.com>
2025-07-28 22:15:06 -0300

    Corrigido erro de hidratação com 'use client' e confirmado CSP

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,23 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  headers: async () => [
-    {
-      source: '/:path*',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self'; connect-src 'self' https://ebook-recomecos-backend.onrender.com https://sandbox.api.pagseguro.com"
-        }
-      ]
-    }
-  ]
+  env: {
+    NEXT_PUBLIC_CSP_HEADER: process.env.NODE_ENV === 'development' ? '' : "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sandbox.pagseguro.uol.com.br;",
+  },
+  async headers() {
+    return process.env.NODE_ENV === 'development'
+      ? []
+      : [
+          {
+            source: '/:path*',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sandbox.pagseguro.uol.com.br;",
+              },
+            ],
+          },
+        ];
+  },
 };
+
 module.exports = nextConfig;
