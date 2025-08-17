bb19ae3f2a6eed2ddf69f4c82145f741d6d965da
wesley <wesleydejesusalvarenga@gmail.com>
2025-08-17 15:14:42 -0300

    Removido checkout do frontend

--- frontend/src/app/components/Nav.jsx
+++ frontend/src/app/components/Nav.jsx
@@ -2,12 +2,10 @@
 import Link from 'next/link';
 
 export default function Nav() {
-  console.log('Nav renderizado'); // Debug
   return (
-    <nav className="bg-white p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        
-      </div>
+    <nav>
+      <a href="/">Home</a>
+      {/* Remova <a href="/checkout">Comprar</a> */}
     </nav>
   );
 }
